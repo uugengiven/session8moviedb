f4fe771fbad77a43ef92def84bcc61abd30a1e83
unknown <lange.john.m@gmail.com>
2019-07-03 15:00:13 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props)
+  {
+    super(props)
+
+    this.state = {
+      movies: []
+    }
+
+    // hey, go grab all of the stuff from that webpage
+    axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b6fbc7f3f313bd395902af464ef47262")
+      .then((response) => {
+        // handle success
+        this.setState({movies: response.data.results}); // this is our movies
+      })
+    console.log('right after that axios get stuff!')
+    // then put it into this.state.movies
+  }
+
+  render() {
+    console.log('Render function!', this.state.movies);
+    return (
+      <div className="App">
+        {this.state.movies.map((movie) => {
+          return (
+          <div>
+            <h1>{movie.title}</h1>
+            <img src={'https://image.tmdb.org/t/p/w500' + movie.poster_path} alt="Movie Poster" />
+            <p>{movie.overview}</p>
+          </div>
+          )
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
